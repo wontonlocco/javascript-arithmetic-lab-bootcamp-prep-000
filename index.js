eb866dacd33c01f18455c41cae89f1701b230edb
Ed Wright <ew_wrightiii@live.com>
2017-12-09 20:26:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,3 +30,10 @@ function makeInt(n) {
   return parseInt(n, 10)
 }
 
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
+
+function preserveDecimal(n) {
+  return parseInt('nonsense!', 10)
+}
